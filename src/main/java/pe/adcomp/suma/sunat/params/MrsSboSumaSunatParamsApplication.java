efa1705a9dfa6c8fcc5cd11e3ab970acb14be551
Daniel Roncal <daniel@ad-comp.net>
2025-11-07 12:26:08 -0500

    feat: Add GraalVM native-maven-plugin configuration and update reflection binding for DTOs

--- src/main/java/pe/adcomp/suma/sunat/params/MrsSboSumaSunatParamsApplication.java
+++ src/main/java/pe/adcomp/suma/sunat/params/MrsSboSumaSunatParamsApplication.java
@@ -1,9 +1,19 @@
 package pe.adcomp.suma.sunat.params;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pe.adcomp.suma.sunat.params.dto.CambioSunatDTO;
+import pe.adcomp.suma.sunat.params.dto.PadronSunatDTO;
+
+import java.util.HashMap;
 
 @SpringBootApplication
+@RegisterReflectionForBinding({
+	PadronSunatDTO.class,
+	CambioSunatDTO.class,
+	HashMap.class
+})
 public class MrsSboSumaSunatParamsApplication {
 
 	public static void main(String[] args) {
